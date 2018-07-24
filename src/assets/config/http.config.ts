3efaa8bf078a202e813b2022d57e426bd23a1069
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2018-07-23 19:07:10 -0500

    Changelog:
    - Improved support for npm compilation

--- src/assets/config/http.config.ts
+++ src/assets/config/http.config.ts
@@ -1,5 +1,5 @@
-export const baseUrl = 'https://localhost:44342/api'; /* development */
-// export const baseUrl = '/api'; /* production */
+// export const baseUrl = 'https://localhost:44342/api'; /* development */
+export const baseUrl = '/api'; /* production */
 
 export let configVariables = {
     authToken : ''
