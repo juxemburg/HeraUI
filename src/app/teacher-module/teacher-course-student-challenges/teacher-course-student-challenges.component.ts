e17773df4148349d47c0547fd31bab33dedf2b5c
Juan Sebastian Montaño Molina <juanmontano@unicauca.edu.co>
2018-10-21 13:16:28 -0500

    Changelog:
    - Added support to see and set the student's next challenge.

--- src/app/teacher-module/teacher-course-student-challenges/teacher-course-student-challenges.component.ts
+++ src/app/teacher-module/teacher-course-student-challenges/teacher-course-student-challenges.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TeacherCourseService } from '../teacher-course/teacher-course.service';
-import { CourseStudentGradeModel } from '../../models/application.teacher.models';
+import { CourseStudentGradeModel, CourseChallengeModel } from '../../models/application.teacher.models';
 import { delay } from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
+import { combineLatest, zip, Observable, forkJoin, Subject } from 'rxjs';
 import { NotificationService } from '../../shared/services/notification.service';
+import { TeacherCourseStudentService } from '../teacher-course/teacher-course-student.service';
 
 @Component({
   selector: 'app-teacher-course-student-challenges',
@@ -15,9 +16,21 @@ export class TeacherCourseStudentChallengesComponent implements OnInit {
 
   public model: CourseStudentGradeModel[];
   public isLoading = false;
+  public nextChallengeId: number;
+  public nextChallenge: CourseChallengeModel;
+
+  private _courseId: number;
+  private _studentId: number;
+
+  private _isSettingChallengeSource = new Subject<boolean>();
+  public isSettingChallenge$ = this._isSettingChallengeSource.asObservable();
+
+  private _challenges$: Observable<CourseStudentGradeModel[]>;
+  private _nextChallenge$: Observable<CourseChallengeModel>;
 
   constructor(
     private _courseService: TeacherCourseService,
+    private _courseStudentService: TeacherCourseStudentService,
     private _route: ActivatedRoute,
     private _notService: NotificationService
   ) { }
@@ -31,16 +44,44 @@ export class TeacherCourseStudentChallengesComponent implements OnInit {
   }
 
   private loadModel(courseId: number, studentId: number) {
+    this._courseId = courseId;
+    this._studentId = studentId;
     this.isLoading = true;
-    this._courseService.GetStudentChallenges(courseId, studentId)
-      .pipe(delay(300))
-      .subscribe(data => {
-        this.isLoading = false;
-        this.model = data;
-      }, err => {
+
+    this._challenges$ = this._courseService.GetStudentChallenges(courseId, studentId)
+      .pipe(delay(300));
+
+    this._challenges$.subscribe(data => this.model = data, err => {
+      this._notService.showError(err);
+      this.isLoading = false;
+    })
+
+    this._nextChallenge$ = this._courseStudentService.GetStudentNextChallenge(courseId, studentId)
+      .pipe(delay(300));
+
+    this._nextChallenge$.subscribe(data => this.nextChallenge = data,
+      err => {
         this._notService.showError(err);
         this.isLoading = false;
       });
+
+    forkJoin(this._challenges$, this._nextChallenge$)
+      .subscribe(_ => this.isLoading = false);
   }
+
+  public setNextChallenge() {
+    this._isSettingChallengeSource.next(true);
+    this._courseStudentService.PostStudentNextChallenge(this._courseId, this._studentId, this.nextChallengeId)
+    .pipe(delay(200))
+    .subscribe(challenge => {
+      this.nextChallenge = challenge;
+      this._isSettingChallengeSource.next(false);
+    }, err => {
+      this._notService.showError(err);
+      this._isSettingChallengeSource.next(false);
+    });
+  }
+
+
 
 }
--- src/app/teacher-module/teacher-course/teacher-course-student.service.ts
+++ src/app/teacher-module/teacher-course/teacher-course-student.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CourseStudentTracesModel } from 'app/models/application.teacher.models';
+import { CourseStudentTracesModel, CourseChallengeModel } from 'app/models/application.teacher.models';
 import { HttpService } from 'app/shared/services/http.service';
 
 @Injectable({
@@ -15,4 +15,13 @@ export class TeacherCourseStudentService {
   public GetStudentTraces(courseId: number, studentId: number): Observable<CourseStudentTracesModel> {
     return this._http.get(`${this._uri}/${courseId}/Student/${studentId}/Traces`);
   }
+
+  public GetStudentNextChallenge(courseId: number, studentId: number): Observable<CourseChallengeModel> {
+    return this._http.get(`${this._uri}/${courseId}/Student/${studentId}/NextChallenge`);
+  }
+
+  public PostStudentNextChallenge(courseId: number, studentId: number, challengeId: number): Observable<any> {
+    return this._http.post(`${this._uri}/${courseId}/Student/${studentId}/NextChallenge/${challengeId}`);
+  }
+
 }
